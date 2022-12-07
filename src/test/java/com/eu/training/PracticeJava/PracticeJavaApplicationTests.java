2881ab2b3f7bcd8f68c2057848ac0c664b361646
Jakir Khan <jakirkhan71@gmail.com>
2022-12-07 12:45:15 +0600

    Innitial Commit

---
+++ src/test/java/com/eu/training/PracticeJava/PracticeJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eu.training.PracticeJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
